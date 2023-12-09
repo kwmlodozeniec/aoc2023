774b7f767cd509f4566686cd01ba4667a490c0ff
kwmlodozeniec <1453791+kwmlodozeniec@users.noreply.github.com>
2023-12-09 09:42:11 +0000

    go.mod for day5

--- day5/main.go
+++ day5/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"regexp"
 	"strconv"
@@ -20,17 +21,11 @@ type seed struct {
 type seeds []seed
 
 type generic_map struct {
+	name              string
 	destination_start int
 	source_start      int
 	length            int
 }
-
-type seed_to_soil generic_map
-type fertilizer_to_water generic_map
-type water_to_light generic_map
-type light_to_temp generic_map
-type temp_to_humidity generic_map
-type humidity_to_location generic_map
 
 func check(e error) {
 	if e != nil {
@@ -38,7 +33,7 @@ func check(e error) {
 	}
 }
 
-func find_numbers(line string) ([]int, error) {
+func find_numbers(line string) []int {
 	found_numbers := []int{}
 
 	numbers := regexp.MustCompile(`\b\d+\b`).FindAllStringIndex(line, -1)
@@ -48,7 +43,7 @@ func find_numbers(line string) ([]int, error) {
 		found_numbers = append(found_numbers, value)
 	}
 
-	return found_numbers, nil
+	return found_numbers
 }
 
 func part1() {
@@ -61,16 +56,30 @@ func part1() {
 	seeds_line := chunks[0]
 	map_data_blocks := chunks[1:]
 
-	// Iterate over the maps
-	seed_to_soil_maps := []seed_to_soil{}
-	fertilizer_to_water_maps := []fertilizer_to_water{}
-	water_to_light_maps := []water_to_light{}
-	light_to_temp_maps := []light_to_temp{}
-	temp_to_humidity_maps := []temp_to_humidity{}
-	humidly_to_location_maps := []humidity_to_location{}
+	// Parse the seeds
+	seeds := find_numbers(seeds_line)
+	fmt.Println(seeds)
 
+	map_list := map[int]string{
+		0: "seed_to_soil",
+		1: "soil_to_fertilizer",
+		2: "fertilizer_to_water",
+		3: "water_to_light",
+		4: "light_to_temp",
+		5: "temp_to_humidity",
+		6: "humidity_to_location",
+	}
+	maps := []generic_map{}
+
+	fmt.Println(map_list, maps)
+
+	// Build the maps
 	for _, map_data := range map_data_blocks {
-		lines := strings.Split(map_data, "\n")
+		lines := strings.Split(map_data, "\n")[1:]
+
+		for _, map_data := range lines {
+
+		}
 	}
 }
 
