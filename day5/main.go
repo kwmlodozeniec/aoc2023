812d87d0937ebd4797d0543851342b2f8246c6fc
kwmlodozeniec <1453791+kwmlodozeniec@users.noreply.github.com>
2023-12-12 21:35:44 +0000

    Day 5 part 1

--- day5/main.go
+++ day5/main.go
@@ -8,25 +8,6 @@ import (
 	"strings"
 )
 
-type seed struct {
-	number     int
-	soil       int
-	fertilizer int
-	water      int
-	light      int
-	humidity   int
-	location   int
-}
-
-type seeds []seed
-
-type generic_map struct {
-	name           string
-	destination_start int
-	source_start      int
-	length            int
-}
-
 func check(e error) {
 	if e != nil {
 		panic(e)
@@ -46,41 +27,54 @@ func find_numbers(line string) []int {
 	return found_numbers
 }
 
+func min(numbers []int) int {
+	min := numbers[0]
+	for _, number := range numbers {
+		if number < min {
+			min = number
+		}
+	}
+	return min
+}
+
 func part1() {
 	// Read the whole file in
-	content, err := os.ReadFile("test_input.txt")
+	content, err := os.ReadFile("input.txt")
 	check(err)
 	// Split on double line break
 	chunks := strings.Split(string(content), "\n\n")
 
-	seeds_line := chunks[0]
-	map_data_blocks := chunks[1:]
-	fmt.Println("Seeds line: ", seeds_line)
-	fmt.Println("Map data blocks: ", map_data_blocks)
-
 	// Parse the seeds
-	seeds := find_numbers(seeds_line)
-	fmt.Println("Seeds: ", seeds)
+	seeds := find_numbers(chunks[0])
 
-	map_list := map[int]string{
-		0: "seed-to-soil",
-		1: "soil-to-fertilizer",
-		2: "fertilizer-to-water",
-		3: "water-to-light",
-		4: "light-to-temperature",
-		5: "temperature-to-humidity",
-		6: "humidity-to-location",
-	}
-	maps := []generic_map{}
-	fmt.Println("Map list: ", map_list, " Maps: ", maps)
+	blocks := chunks[1:]
 
-	// Build the maps
-	for _, map_data := range map_data_blocks {
-		lines := strings.Split(map_data, "\n")
-		map_name := strings.Split(lines[0], " map:")[0]
-		map_data_lines := lines[1:]
-		fmt.Println("Map name: ", map_name, " Map data lines: ", map_data_lines)
+	// Parse the map data
+	for _, block := range blocks {
+		ranges := [][]int{}
+		for _, line := range strings.Split(block, "\n")[1:] {
+			if len(line) > 0 {
+				ranges = append(ranges, find_numbers(line))
+			}
+		}
+		new_locations := []int{}
+		for _, seed := range seeds {
+			found := false
+			for _, r := range ranges {
+				dest_start, seed_start, length := r[0], r[1], r[2]
+				if seed_start <= seed && seed < seed_start+length {
+					new_locations = append(new_locations, seed-seed_start+dest_start)
+					found = true
+					break
+				}
+			}
+			if !found {
+				new_locations = append(new_locations, seed)
+			}
+		}
+		seeds = new_locations
 	}
+	fmt.Println("Part 1: ", min(seeds))
 }
 
 func part2() {
